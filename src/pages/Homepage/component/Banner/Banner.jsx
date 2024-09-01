52b42d921d607cdfffe8d1ff7a8b7cc6135e3b99
JeongChan2 <jcstar55@gmail.com>
2024-09-01 22:28:32 +0900

    feat: random banner

--- src/pages/Homepage/component/Banner/Banner.jsx
+++ src/pages/Homepage/component/Banner/Banner.jsx
@@ -7,6 +7,14 @@ const Banner = () => {
 
 
   const {data, isLoading, isError, error} = usePopularMoviesQuery();
+
+
+  const getRandomNumber = () => {
+    return Math.floor(Math.random() * 20); // 0부터 19까지의 랜덤한 숫자 생성
+  };
+
+  const randNum = getRandomNumber();
+
   if(isLoading){
     return <h1>Loading...</h1>
   }
@@ -16,12 +24,12 @@ const Banner = () => {
   console.log(data)
   return (
     <div style={{
-      backgroundImage:`url(https://image.tmdb.org/t/p/original${data?.results[0].poster_path})`
+      backgroundImage:`url(https://image.tmdb.org/t/p/original${data?.results[randNum].poster_path})`
     }} className='banner'>
       
       <div className='text-white banner-text-area'>
-        <h1>{data?.results[0].title}</h1>
-        <p>{data?.results[0].overview}</p>
+        <h1>{data?.results[randNum].title}</h1>
+        <p>{data?.results[randNum].overview}</p>
       </div>
     </div>
   )
