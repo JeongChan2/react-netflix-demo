b42e286d375b2ccdb89a8c191ade869c166a5999
JeongChan2 <jcstar55@gmail.com>
2024-09-03 20:32:10 +0900

    folder struct update

---
+++ src/common/MovieSlider/MovieSlider.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './MovieSlider.style.css'
+import Carousel from "react-multi-carousel";
+import MovieCard from '../MovieCard/MovieCard';
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 6,
+    slidesToSlide: 3 // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    slidesToSlide: 2 // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1 // optional, default to 1.
+  }
+};
+
+const MovieSlider = ({subTitle, movies, genresList}) => {
+  return (
+    <div>
+      <h3 className="color-white">{subTitle}</h3>
+      <Carousel
+        infinite={true}
+        centerMode={true}
+        itemClass="movie-slider p-1"
+        containerClass="carousel-container"
+        responsive={responsive}
+      >
+        {movies.map((movie,index)=><MovieCard movie={movie} key={index} genresList={genresList}/>)}
+        
+      </Carousel>
+    </div>
+  )
+}
+
+export default MovieSlider
--- src/pages/Homepage/component/MovieSlide/MovieSlide.jsx
+++ src/pages/Homepage/component/MovieSlide/MovieSlide.jsx
@@ -1,28 +1,7 @@
 import React from "react";
 import { Alert } from "bootstrap";
-import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import MovieCard from "../MovieCard/MovieCard";
-import './MovieSlide.style.css'
-
-
-const responsive = {
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 6,
-    slidesToSlide: 3 // optional, default to 1.
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2,
-    slidesToSlide: 2 // optional, default to 1.
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-    slidesToSlide: 1 // optional, default to 1.
-  }
-};
+import MovieSlider from "../../../../common/MovieSlider/MovieSlider";
 
 
 
@@ -36,17 +15,7 @@ const MovieSlide = ({usehook, subTitle, genresList}) => {
   }
   return (
     <div>
-      <h3 className="color-white">{subTitle}</h3>
-      <Carousel
-        infinite={true}
-        centerMode={true}
-        itemClass="movie-slider p-1"
-        containerClass="carousel-container"
-        responsive={responsive}
-      >
-        {data.results.map((movie,index)=><MovieCard movie={movie} key={index} genresList={genresList}/>)}
-        
-      </Carousel>
+      <MovieSlider subTitle={subTitle} movies={data.results} genresList={genresList} />
       
     </div>
   );
