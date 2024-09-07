6edb813ebd1d12597801729844a784877b6661d4
JeongChan2 <jcstar55@gmail.com>
2024-09-07 10:46:52 +0900

    feat: movie sort

--- src/hooks/useSearchMovie.js
+++ src/hooks/useSearchMovie.js
@@ -7,9 +7,9 @@ const fetchSearchMovie=({keyword, page})=>{
   : api.get(`/movie/popular?language=ko-KR&page=${page}`);
 }
 
-export const useSearchMovieQuery=({keyword, page}) => {
+export const useSearchMovieQuery=({keyword, page, sortByAsc}) => {
   return useQuery({
-    queryKey:['move-search', {keyword, page}],
+    queryKey:['move-search', {keyword, page, sortByAsc}],
     queryFn: () =>fetchSearchMovie({keyword, page}),
     select:(result)=>result.data
   })
--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -12,13 +12,20 @@ const MoviePage = () => {
   const keyword = query.get("q");
   const [page, setPage] = useState(1);
   const [genre, setGenre] = useState(null);
+  const [sortByAsc, setSortByAsc] = useState(false);
 
   const { data, isLoading, isError, error } = useSearchMovieQuery({
     keyword,
     page,
+    sortByAsc,
   });
 
-  const { data:genreList, isLoading:genIsLoading, isError:genIsError, error:genError } = useGenresListMoviesQuery();
+  const {
+    data: genreList,
+    isLoading: genIsLoading,
+    isError: genIsError,
+    error: genError,
+  } = useGenresListMoviesQuery();
 
   const handlePageClick = ({ selected }) => {
     setPage(selected + 1);
@@ -28,14 +35,49 @@ const MoviePage = () => {
     setPage(1);
   }, [keyword]);
 
+  // 영화 목록을 정렬하는 함수
+  const sortedMovies = (movies) => {
+    return movies?.sort((a, b) => {
+      return sortByAsc
+        ? a.popularity - b.popularity
+        : b.popularity - a.popularity;
+    });
+  };
+
   return (
     <Container>
       <Row>
         <Col lg={4} xs={12}>
-          {genreList?.genres.map((id,index) => {
-            console.log((id.id))
-            return <Badge onClick={() => setGenre(id.id)} className="genre-button" bg="danger" key={index}>{id.name}</Badge>
-          })}
+          <Col>
+            <Badge
+              onClick={() => setSortByAsc(false)}
+              className="popularity-button"
+              bg="primary"
+            >
+              Popularity(Desc)
+            </Badge>
+            <Badge
+              onClick={() => setSortByAsc(true)}
+              className="popularity-button"
+              bg="primary"
+            >
+              Popularity(Asc)
+            </Badge>
+          </Col>
+          <Col>
+            {genreList?.genres.map((id, index) => {
+              return (
+                <Badge
+                  onClick={() => setGenre(id.id)}
+                  className="genre-button"
+                  bg="danger"
+                  key={index}
+                >
+                  {id.name}
+                </Badge>
+              );
+            })}
+          </Col>
         </Col>
 
         <Col lg={8} xs={12}>
@@ -44,50 +86,48 @@ const MoviePage = () => {
           ) : (
             <>
               <Row>
-                {genre?(data?.results.filter((movie) => {
-                    return (
-                      movie.genre_ids.includes(genre)
-                    );
-                    
-
-                })).map((movie, index) => {
-              
-                  return (
-                    <Col key={index} lg={4} xs={12}>
-                      <MovieCard movie={movie} size="home"/>
-                    </Col>
-                    )
-
-              }):data?.results.map((movie, index) => {
-              
-                  return (
-                    <Col key={index} lg={4} xs={12}>
-                      <MovieCard movie={movie} size="home"/>
-                    </Col>
-                    )
-
-              })}
+                {genre
+                  ? sortedMovies(
+                      data?.results.filter((movie) =>
+                        movie?.genre_ids.includes(genre)
+                      )
+                    )?.map((movie, index) => {
+                      return (
+                        <Col key={index} lg={4} xs={12}>
+                          <MovieCard movie={movie} size="home" />
+                        </Col>
+                      );
+                    })
+                  : sortedMovies(data?.results)?.map((movie, index) => {
+                      return (
+                        <Col key={index} lg={4} xs={12}>
+                          <MovieCard movie={movie} size="home" />
+                        </Col>
+                      );
+                    })}
               </Row>
-              {!genre && (<ReactPaginate
-                nextLabel="next >"
-                onPageChange={handlePageClick}
-                pageRangeDisplayed={3}
-                marginPagesDisplayed={2}
-                pageCount={data?.total_pages}
-                forcePage={page - 1}
-                previousLabel="< previous"
-                pageClassName="page-item"
-                pageLinkClassName="page-link"
-                previousClassName="page-item"
-                previousLinkClassName="page-link"
-                nextClassName="page-item"
-                nextLinkClassName="page-link"
-                breakLabel="..."
-                breakClassName="page-item"
-                breakLinkClassName="break-link"
-                containerClassName="pagination"
-                activeClassName="active"
-              />)}
+              {!genre && (
+                <ReactPaginate
+                  nextLabel="next >"
+                  onPageChange={handlePageClick}
+                  pageRangeDisplayed={3}
+                  marginPagesDisplayed={2}
+                  pageCount={data?.total_pages}
+                  forcePage={page - 1}
+                  previousLabel="< previous"
+                  pageClassName="page-item"
+                  pageLinkClassName="page-link"
+                  previousClassName="page-item"
+                  previousLinkClassName="page-link"
+                  nextClassName="page-item"
+                  nextLinkClassName="page-link"
+                  breakLabel="..."
+                  breakClassName="page-item"
+                  breakLinkClassName="break-link"
+                  containerClassName="pagination"
+                  activeClassName="active"
+                />
+              )}
             </>
           )}
         </Col>
